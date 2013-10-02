c699a4f804abf3bbc6810ce49e06458db15d7bef
Marten Veldthuis <marten@veldthuis.com>
2013-10-03 00:17:23 +0200

    second submission

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -1,22 +1,39 @@
+class Nucleotide < Struct.new(:char)
+  DNA = %w(A C G T)
+  RNA = %w(A C G U)
+
+  def valid?
+    dna? or rna?
+  end
+
+  def dna?
+    DNA.include? char
+  end
+
+  def rna?
+    RNA.include? char
+  end
+end
+
 class DNA
+
   def initialize(sequence)
-    raise ArgumentError unless sequence =~ /\A[ACTG]*\Z/
+    sequence = sequence.chars.map {|i| Nucleotide.new(i) }
+    raise ArgumentError if sequence.find {|i| not i.dna? }
 
     @sequence = sequence
   end
 
   def count(nucleotide)
-    raise ArgumentError unless ['A', 'C', 'T', 'G', 'U'].include?(nucleotide)
+    nucleotide = Nucleotide.new(nucleotide)
+    raise ArgumentError unless nucleotide.valid?
 
-    @sequence.chars.select {|i| i == nucleotide }.size
+    @sequence.select {|i| i == nucleotide }.size
   end
 
   def nucleotide_counts
-    {
-      'A' => count('A'),
-      'T' => count('T'),
-      'C' => count('C'),
-      'G' => count('G')
-    }
+    Nucleotide::DNA.each_with_object({}) do |nucleotide, counts|
+      counts[nucleotide] = count(nucleotide)
+    end
   end
 end
