a5973fb970170b0945ff378ca1370c52f58542f7
Marten Veldthuis <marten@veldthuis.com>
2013-10-04 13:16:35 +0200

    reformat

--- ruby/point-mutations/dna.rb
+++ ruby/point-mutations/dna.rb
@@ -4,6 +4,7 @@ class DNA
   end
 
   def hamming_distance(other_sequence)
-    @sequence.chars.zip(other_sequence.chars).count {|a,b| a && b && a != b }
+    @sequence.chars.zip(other_sequence.chars)
+                   .count {|a,b| a and b and a != b }
   end
 end
