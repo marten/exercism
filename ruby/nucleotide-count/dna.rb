10503d756a6fff216076041674bea7c4ec70802c
Marten Veldthuis <marten@veldthuis.com>
2013-10-04 12:35:16 +0200

    version 3

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -15,25 +15,31 @@ class Nucleotide < Struct.new(:char)
   end
 end
 
+module DNAAdapter
+  def initialize(sequence)
+    super(sequence.chars.map{|char| Nucleotide.new(char) })
+  end
+  def count(nucleotide)
+    super(Nucleotide.new(nucleotide))
+  end
+end
+
 class DNA
+  prepend DNAAdapter
 
   def initialize(sequence)
-    sequence = sequence.chars.map {|i| Nucleotide.new(i) }
     raise ArgumentError if sequence.find {|i| not i.dna? }
-
     @sequence = sequence
   end
 
   def count(nucleotide)
-    nucleotide = Nucleotide.new(nucleotide)
     raise ArgumentError unless nucleotide.valid?
-
     @sequence.select {|i| i == nucleotide }.size
   end
 
   def nucleotide_counts
-    Nucleotide::DNA.each_with_object({}) do |nucleotide, counts|
-      counts[nucleotide] = count(nucleotide)
+    Nucleotide::DNA.each_with_object({}) do |nucleotide, nucleotide_counts|
+      nucleotide_counts[nucleotide] = count(nucleotide)
     end
   end
 end
